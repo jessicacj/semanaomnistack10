43e0d77c615e3b21c3a0940a99582def90160a2e
jessicacj <jessicacj@outlook.com>
2020-01-21 13:17:18 -0300

    edit style and app

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = 'app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class = "input block">
+          <label htmlFor = "">Usuário do GitHub</label>
+          <input name = "github_user" id = "user_github" required />
+          </div>
+
+          <div class = "input block">
+          <label htmlFor = "tecnologias">Tecnologias</label>
+          <input name = "tecnologias" id = "tecnologias" required />
+          </div>
+
+          <div className = "input-group">
+            <div class = "input-block">
+              <label htmlFor = "latitude">Latitude</label>
+              <input name = "latitude" id = "latitude" required />
+            </div>
+
+            <div class = "input-block">
+              <label htmlFor = "longitude">Longitude</label>
+              <input name = "longitude" id = "longitude" required />
+            </div>
+          </div>
+          
+          <button type = "submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
     </div>
+    
   );
 }
 
